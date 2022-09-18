79408d7d6cb6ad6cb79133f4d7f5eeafc39301aa
Jack <me@jackmerrill.com>
2022-09-17 23:01:34 -0400

    fix: rework workspace member code

--- api/v1/workspaces/controller.go
+++ api/v1/workspaces/controller.go
@@ -10,19 +10,20 @@ func WorkspacesHandler(router fiber.Router) {
 	authed.Get("/", GetWorkspaces)
 	authed.Post("/", CreateWorkspace)
 
-	workspaceRouter := authed.Group("/:workspace_id").Use(utils.WorkspaceMiddleware, utils.WorkspaceMemberMiddleware)
+	workspaceRouter := authed.Group("/:workspace_id").Use(utils.WorkspaceMiddleware)
 
 	workspaceRouter.Get("/", GetWorkspace)
 	workspaceRouter.Put("/", UpdateWorkspace)
 	workspaceRouter.Post("/", UpdateWorkspace)
 	// workspaceRouter.Delete("/:id", DeleteWorkspace)
 
-	workspaceRouter.Get("/members", GetWorkspaceMembers)
-	workspaceRouter.Post("/members", AddWorkspaceMember)
-	workspaceRouter.Get("/members/:member", GetWorkspaceMember)
-	workspaceRouter.Put("/members/:member", UpdateWorkspaceMember)
-	workspaceRouter.Delete("/members/:member", RemoveWorkspaceMember)
-	workspaceRouter.Post("/members/:member/remove", RemoveWorkspaceMember) // Fallback for HTML Forms
+	memberRouter := workspaceRouter.Group("/members").Use(utils.WorkspaceMemberMiddleware)
+	memberRouter.Get("/", GetWorkspaceMembers)
+	memberRouter.Post("/", AddWorkspaceMember)
+	memberRouter.Get("/:member", GetWorkspaceMember)
+	memberRouter.Put("/:member", UpdateWorkspaceMember)
+	memberRouter.Delete("/:member", RemoveWorkspaceMember)
+	memberRouter.Post("/:member/remove", RemoveWorkspaceMember) // Fallback for HTML Forms
 
 	// compatablity with HTML forms
 	workspaceRouter.Post("/bot/create", CreateWorkspaceBot)
